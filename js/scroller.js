66415f880843c576a22f1259e9d0f5add58d8b18
Brendan Whitfield <brendanw@windworksdesign.com>
2014-10-09 22:23:26 -0400

    fixed scrolling in chrome

--- js/scroller.js
+++ js/scroller.js
@@ -18,6 +18,8 @@ window.__exeJS__.scroller = function() {
 	var tiny_top;
 	var tiny_bottom;
 
+	var mdown = false; //chrome doesn't persist button states during move events...
+
 	function setup()
 	{
 		h_window = window.innerHeight;
@@ -27,6 +29,9 @@ window.__exeJS__.scroller = function() {
 
 		var h_line      = document.querySelector("#code pre").clientHeight;
 		var h_tiny_line = document.querySelector("#tiny pre").clientHeight;
+
+		//hack for chrome (dimensions haven't been computed yet)
+		if(!h_tiny) h_tiny = document.querySelector("#tiny code").childElementCount * h_tiny_line;
 
 		//compute the zoom ratio
 		zoom = h_line / h_tiny_line;
@@ -64,7 +69,7 @@ window.__exeJS__.scroller = function() {
 	//update display
 	function scrollBar(e)
 	{
-		var tiny_px = map(e.pageY, 0, body_overflow, tiny_top, tiny_bottom);
+		var tiny_px = map(window.pageYOffset, 0, body_overflow, tiny_top, tiny_bottom);
 		
 		frame.style.top = (tiny_px - tiny_top) + "px";
 
@@ -80,9 +85,15 @@ window.__exeJS__.scroller = function() {
 
 	//event handlers
 	window.onscroll = scrollBar;
-	tiny.onmousedown = scrollTiny;
+	window.onmouseup = function(e) {
+		mdown = false; //chrome fix
+	}
+	tiny.onmousedown = function(e) {
+		mdown = true; //chrome fix
+		scrollTiny(e);
+	};
 	tiny.onmousemove = function(e) {
-		if(e.buttons === 1) //if left mouse down
+		if(mdown) //if left mouse down
 			scrollTiny(e);
 	};
 
